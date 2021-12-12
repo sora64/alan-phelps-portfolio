c1e9dc0cae63e36f04dc9393bdcd74055ffc7e56
Alan Phelps <phelpsa64@gmail.com>
2021-12-11 20:46:03 -0500

    added real hamburger

--- Assets/portfolio.js
+++ Assets/portfolio.js
@@ -1,10 +1,18 @@
-// show and hide the navigation menu links when the user clicks on the hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-function hamburgerF() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
 }
